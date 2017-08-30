d803fa6e17a120b31d9dcab13e47b87d6156bdd7
Venom <home@bum.com>
2017-08-30 17:05:58 +0300

    готово

--- Collection/src/PhoneBook.java
+++ Collection/src/PhoneBook.java
@@ -1,30 +1,28 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 
 public class PhoneBook {
     ArrayList<Object>key=new ArrayList <> (  );
     HashMap <Object, Integer> phoneBook = new HashMap <> ();
+    int index = 0;
 
     public void add(/*HashMap <String, Integer> phoneBook,*/ String surname, int number) {
         key.add ( surname );
-        System.out.println (" key.indexOf (surname) "+ surname + " "+ key.indexOf (surname));
-        phoneBook.put ( key.indexOf (surname), number);
-        System.out.println (phoneBook.get(key.indexOf ( surname )));
-
+        phoneBook.put ( index, number);
+        index++;
     }
 
     public void get(/*HashMap <String, Integer> phoneBook,*/String surname) {
         if (key.contains( surname )) {
             int i=0;
            while(i < phoneBook.size()){
-              if(phoneBook.containsKey ( key.indexOf ( surname ) )&&key.get( i ).equals ( surname ))
-                System.out.println ( "Номер абонента под фамилией: " + surname + ": " + phoneBook.get(i));
+              if(key.get( i ).equals ( surname ))
+                System.out.println ( "Номер абонента под фамилией: " + key.get(i) + ": " + phoneBook.get(i));
                i++;
                ;
            }
-            System.out.println (surname + phoneBook.get(3));
-            System.out.println (surname + phoneBook.get(5));
+        }else{
+            System.out.println ("Номера абонента с фамилией "+ surname + "не существует.");
         }
     }
 }
