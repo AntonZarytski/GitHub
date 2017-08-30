4fd1605d7823abc505c086843e2716b23145b8ae
Venom <home@bum.com>
2017-08-30 16:44:26 +0300

    в процессе

--- Collection/src/Main1.java
+++ Collection/src/Main1.java
@@ -4,11 +4,13 @@ public class Main1 {
         phoneBook.add ( "Зарицкий", 5939795 );
         phoneBook.add ( "Махонь", 2168033 );
         phoneBook.add ( "Пушкин", 4545434 );
+        phoneBook.add ( "Иванов", 14849122 );
         phoneBook.add ( "Дудник", 4711904 );
         phoneBook.add ( "Иванов", 2345789 );
         phoneBook.add ( "Титов", 2975454 );
         phoneBook.add ( "Сидоров", 4717461 );
-        phoneBook.add ( "Иванов", 14849122 );
+        phoneBook.add ( "Иванов", 1117892 );
+
         phoneBook.get ( "Зарицкий" );
         phoneBook.get ("Пупкин");
         phoneBook.get ("Иванов");
--- Collection/src/PhoneBook.java
+++ Collection/src/PhoneBook.java
@@ -1,20 +1,31 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class PhoneBook {
-    HashMap <String, Integer> phoneBook= new HashMap <> (  );
-    public PhoneBook(){
-        HashMap <String, Integer> phoneBook= new HashMap <> (  );
+    ArrayList<Object>key=new ArrayList <> (  );
+    HashMap <Object, Integer> phoneBook = new HashMap <> ();
+
+    public void add(/*HashMap <String, Integer> phoneBook,*/ String surname, int number) {
+        key.add ( surname );
+        System.out.println (" key.indexOf (surname) "+ surname + " "+ key.indexOf (surname));
+        phoneBook.put ( key.indexOf (surname), number);
+        System.out.println (phoneBook.get(key.indexOf ( surname )));
+
     }
 
-    public void add(/*HashMap <String, Integer> phoneBook,*/ String surname, int number ){
-        phoneBook.put(surname,number);
-    }
     public void get(/*HashMap <String, Integer> phoneBook,*/String surname) {
-        if (phoneBook.containsKey ( surname )) {
-            System.out.println ( "Номер абонента под фамилией: " + surname + ": " + phoneBook.get ( surname ) );
-        }else{
-            System.out.println ("Абонента под фамилией " + surname + " в справочнике не найдено");
+        if (key.contains( surname )) {
+            int i=0;
+           while(i < phoneBook.size()){
+              if(phoneBook.containsKey ( key.indexOf ( surname ) )&&key.get( i ).equals ( surname ))
+                System.out.println ( "Номер абонента под фамилией: " + surname + ": " + phoneBook.get(i));
+               i++;
+               ;
+           }
+            System.out.println (surname + phoneBook.get(3));
+            System.out.println (surname + phoneBook.get(5));
         }
     }
-
 }
+
