f42bc43764db4f096fb401cbc4f8ba792e1ed538
Venom <30825272+AntonZarytski@users.noreply.github.com>
2017-09-01 17:47:10 +0300

    коллекции готово

--- Collection/src/PhoneBook.java
+++ Collection/src/PhoneBook.java
@@ -1,29 +1,18 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PhoneBook {
-    ArrayList<Object>surname=new ArrayList <> (  );
-    ArrayList<Object>number=new ArrayList<>();
-
+    HashMap<String, ArrayList <Integer>> phoneBook = new HashMap<>();
     int index = 0;
-
-    public void add(String surname, int number) {
-        if(this.surname.contains(surname)){
-            Object number1 = this.number.remove(this.surname.indexOf(surname));
-            index--;
-            this.number.add(this.surname.indexOf(surname),number1 + " " + number);
-        }else {
-            this.surname.add(index, surname);
-            this.number.add(index, number);
-            index++;
+    public void add(String surname, Integer number) {
+        if (!phoneBook.containsKey(surname)) {
+            phoneBook.put(surname, new ArrayList<>());
         }
+        phoneBook.get(surname).add(number);
     }
-
-    public void get(String surname) {
-        if (this.surname.contains(surname)){
-            System.out.println("Номер(а) абонента под фамилией: " + this.surname.get(this.surname.indexOf(surname)) + ": " + this.number.get(this.surname.indexOf(surname)));
-        }else {
-            System.out.println ("Номера абонента с фамилией "+ surname + " не существует.");
+    public void get(String surname){
+        if (phoneBook.containsKey(surname)){
+            System.out.println("Номер(а) абонента под фамилией: "+ surname + ": " + phoneBook.get(surname));
         }
     }
 }
-
