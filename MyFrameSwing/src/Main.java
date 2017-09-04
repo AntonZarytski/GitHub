df7f974b4f2f7be3c5a01dcaf042e36f59cbe5cb
Venom <30825272+AntonZarytski@users.noreply.github.com>
2017-09-04 20:15:49 +0300

    add JFrame

--- MyFrameSwing/src/Main.java
+++ MyFrameSwing/src/Main.java
@@ -1,11 +1,8 @@
-import javax.swing.*;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        //Frame fr = new Frame("window");
          JFrame2 fr2 = new JFrame2("Окно чата");
-       //JFrame3 ft3 = new JFrame3("Окно чата");
-
     }
 }
