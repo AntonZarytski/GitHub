9109aed27234d2819ca6d2397d19ca0912bc30d4
Venom <home@bum.com>
2017-09-07 13:42:52 +0300

    commit1

--- MultThreading/src/Main.java
+++ MultThreading/src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
     TestClass tc = new TestClass ();
-       /* tc.method1 ();
-      tc.method2 ();
-        tc.method3 ();*/
+        tc.method1 ();
+        tc.method2 ();
+        tc.method3 ();
     new Thread ( tc::method1 ).start ();
     new Thread ( tc::method2 ).start ();
     new Thread ( tc::method3 ).start ();
--- MultThreading/src/TestClass.java
+++ MultThreading/src/TestClass.java
@@ -3,24 +3,23 @@ public class TestClass {
     private static final int H = SIZE/2;
     private static final int G = SIZE/4;
 
-    float[] arr= new float[SIZE];
+    float[] arr;
     float[] arr1=new float[H];
     float [] arr2=new float[H];
     float [] arr3=new float[G];
     float [] arr4=new float[G];
     float [] arr5=new float[G];
     float [] arr6=new float[G];
-    public synchronized void method1(){
-        System.out.println ("1");
+    public void method1(){
+        arr=new float[SIZE];
         for(int i = 0; i < arr.length; i++){
             arr[i] = 1;
         }
         long a = System.currentTimeMillis();
         useArray ( arr );
-        System.out.println(System.currentTimeMillis() - a);
+        System.out.println("1й метод выполнялся "+ (System.currentTimeMillis() - a));
     }
-    public synchronized void method2(){
-        System.out.println ("2");
+    public void method2(){
         arr=new float[SIZE];
         for(int i = 0; i < arr.length; i++){
             arr[i] = 1;
@@ -32,10 +31,10 @@ public class TestClass {
         useArray (arr2);
         System.arraycopy ( arr1,0,arr,0,H );
         System.arraycopy ( arr2,0, arr, H, H);
-        System.out.println(System.currentTimeMillis() - a);
+        System.out.println("2й метод выполнялся "+ (System.currentTimeMillis() - a));
     }
-    public synchronized void method3(){
-        System.out.println ("3");
+    public void method3(){
+
         arr=new float[SIZE];
         for(int i = 0; i < arr.length; i++){
             arr[i] = 1;
@@ -48,12 +47,12 @@ public class TestClass {
         useArray (arr3);
         useArray (arr4);
         useArray (arr5);
-        useArray (arr6 );
+        useArray (arr6);
         System.arraycopy ( arr3,0,arr,0,G);
         System.arraycopy ( arr4,0, arr, G, G);
         System.arraycopy ( arr5,0, arr, H, G);
         System.arraycopy ( arr6,0, arr, G*3, G);
-        System.out.println(System.currentTimeMillis() - a);
+        System.out.println("3й метод выполнялся "+ (System.currentTimeMillis() - a));
     }
     public void useArray( float arr[]){
         for(int i = 0; i < arr.length; i++){
